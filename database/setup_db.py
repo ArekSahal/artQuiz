2ba748fd24667d084800273c8d6580a8c7525cc2
einarlennelov <einar.lennelov@doctrin.se>
2020-06-19 10:11:36 +0200

    Add config file

--- database/setup_db.py
+++ database/setup_db.py
@@ -1,9 +1,11 @@
+import config
 import sqlalchemy as sa
 import sqlalchemy_utils as sau
 from database.schema import metadata
 
-engine = sa.create_engine('mysql+pymysql://root:password@localhost:3306/quizbot')
-if not sau.database_exists(engine.url):
-    sau.create_database(engine.url)
+if __name__ == "__main__":
+    engine = sa.create_engine(config.DATABASE_URL)
+    if not sau.database_exists(engine.url):
+        sau.create_database(engine.url)
 
-metadata.create_all(engine)
+    metadata.create_all(engine)
